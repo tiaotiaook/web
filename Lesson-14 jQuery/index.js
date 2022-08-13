f60019dd616412888c278fa734f620dd0b0ac595
tiaotiaook <shihuaok@gmail.com>
2022-08-14 00:57:11 +0800

    Lesson-14 jQuery

---
+++ Lesson-14 jQuery/index.js
@@ -0,0 +1,12 @@
+$("h1").addClass("big-title margin-50");
+
+
+
+$("button");
+
+// console.log($("h1").css("font-size"));
+
+// document.querySelector("h1");
+// $("h1");
+//
+// document.querySelectorAll("button");
