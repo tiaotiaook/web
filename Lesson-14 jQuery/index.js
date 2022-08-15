b4acc13bccd88a04acbd30648b076d188ebcbf71
tiaotiaook <shihuaok@gmail.com>
2022-08-15 23:57:36 +0800

    Lesson -14 jQuery listener

--- Lesson-14 jQuery/index.js
+++ Lesson-14 jQuery/index.js
@@ -1,8 +1,46 @@
-$("h1").addClass("big-title margin-50");
+
+$(document).keypress(function(event){
+  $("h1").text(event.key);
+});
+
+
+// $("input").keypress(function(event){
+//   $("h1").text(event.key);
+// });
 
 
 
-$("button");
+
+// for (var i=0;i<5;i++){
+//   document.querySelectorAll("button")[i].addEventListener("click",function(){
+//   document.querySelector("h1").style.color="purple";
+//   });
+// }
+
+// $("button").click(function(){
+//   $("h1").css("color","purple");
+// });
+
+
+
+
+// $("h1").click(function(){
+//   $("h1").css("color","purple");
+// });
+
+// $("a").attr("href","https://www.yahoo.com");
+
+// $("h1").text("Bye");
+
+// $("button").text("don't click me");
+// $("button").html("<em>hey<em>");
+
+
+
+// $("h1").addClass("big-title margin-50");
+//
+//
+// $("button");
 
 // console.log($("h1").css("font-size"));
 
