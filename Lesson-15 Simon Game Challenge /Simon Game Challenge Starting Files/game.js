f7f3492bfb00318e69dc0bec7cec86de13f9f457
tiaotiaook <shihuaok@gmail.com>
2022-08-17 23:49:26 +0800

    Lesson-15 too difficult !!!

--- Lesson-15 Simon Game Challenge /Simon Game Challenge Starting Files/game.js
+++ Lesson-15 Simon Game Challenge /Simon Game Challenge Starting Files/game.js
@@ -1,28 +1,42 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-// console.log(buttonColours[1]);
+
+
+var userClickedPattern = [];
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColour);
+
+  console.log(userClickedPattern);
+  });
+
+
+
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
-  // console.log(randomNumber);
+
   var randomChosenColour = buttonColours[randomNumber];
 
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
 
 
-}
+};
 
 
 
 
-nextSequence()
+// nextSequence()
 //
 // console.log(gamePattern);
 //
