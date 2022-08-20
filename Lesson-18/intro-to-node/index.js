c7aff85de60d2f6cfaeb7e58b054c8422adaaab3
tiaotiaook <shihuaok@gmail.com>
2022-08-20 23:08:43 +0800

    Lesson-19 Express.js

---
+++ Lesson-18/intro-to-node/index.js
@@ -0,0 +1,7 @@
+var superheroes=require("superheroes");
+var mysuperheroname=superheroes.random();
+console.log(mysuperheroname);
+
+var supervillains=require("supervillains");
+var mysupervillainsname=supervillains.random();
+console.log(mysupervillainsname);
