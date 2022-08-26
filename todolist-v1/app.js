adec6fd03f16b050448cf3115827c41ca0cfc917
tiaotiaook <shihuaok@gmail.com>
2022-08-26 23:53:08 +0800

    Lesson 22 ejs

---
+++ todolist-v1/app.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+
+const app=express();
+
+app.get("/",function(req,res){
+  var today = new Date();
+
+  var currentDay=today.getDay;
+
+  if (currentDay===6 || currentDay===0){
+    res.write("<h1>yay it's the weekend!</h1>");
+  }else{
+    res.sendFile(__dirname+"/index.html");
+  }
+});
+
+app.listen(3000,function(){
+  console.log("server started on port 3000");
+})
