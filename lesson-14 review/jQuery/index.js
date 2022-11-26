6956a40c403a86dfd58b0a088b89ed98cdce409b
tiaotiaook <shihuaok@gmail.com>
2022-11-27 02:05:39 +0800

    lesson-13 review jQuery

---
+++ lesson-14 review/jQuery/index.js
@@ -0,0 +1,38 @@
+
+$("button").on("click",function(){
+    $("h1").slideUp().slideDown().animate({opacity:0.5});
+    // $("h1").animate({opacity:0.5});
+    // $("h1").animate({opacity:1});
+    // $("h1").fadeOut();
+    // $("h1").fadeIn();
+    // $("h1").fadeToggle();
+})
+
+
+
+// $("h1").click(function(){
+//     $("h1").css("color","red")
+// })
+
+// $("button").click(function(){
+//     $("h1").css("color","green") ;
+// })
+
+// $("input").keypress(function(event){
+//     $("h1").text(event.key)
+//     // console.log(event.key);
+// })
+
+// $("h1").on("mouseover",function(){
+//     $("h1").css("color","pink")
+// })
+
+
+
+// $("h1").addClass("big-title margin-50");
+// $("h1").text("good bye");
+
+// // $("button").text("don't touch me")
+// $("button").html("<em>hey</em>")
+
+// $("a").attr("href","https://www.yahoo.com")
